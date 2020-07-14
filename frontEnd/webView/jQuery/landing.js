2495bbf27290df6c17b7234dd300d99f3fe9f11f
John Burns <johnb1003@gmail.com>
2020-07-14 19:30:23 -0400

    redirect to login and signup

--- frontEnd/webView/jQuery/landing.js
+++ frontEnd/webView/jQuery/landing.js
@@ -1,6 +1,9 @@
 // Regular expression to check valid emails. Source: http://emailregex.com/
 const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+// Regular expression to check password is only alphanumeric values and length is between 5-20 inclusive. Source: https://stackoverflow.com/questions/4745112/javascript-regex-for-alphanumeric-string-with-length-of-3-5-chars
+const passwordRegEx = /^([a-zA-Z0-9]){5,20}$/;
+
 const baseAPIURL = "https://reflectme.tech/api/v1";
 
 function setCookie(key, value) {
