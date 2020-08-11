f6ffa616ab33b825fc5df11ed3ad47fea2861055
John Burns <johnb1003@gmail.com>
2020-08-11 16:20:49 -0400

    Changing persistance method

--- backEnd/src/main/java/com/reflectme/server/PersistenceConfig.java
+++ backEnd/src/main/java/com/reflectme/server/PersistenceConfig.java
@@ -3,6 +3,7 @@ package com.reflectme.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.instrument.classloading.ReflectiveLoadTimeWeaver;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
@@ -16,15 +17,15 @@ public class PersistenceConfig {
     DataSource dataSource;
 
     @Bean
-    public EntityManagerFactory emf(){
+    public LocalContainerEntityManagerFactoryBean emf(){
 
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
         emf.setPersistenceProviderClass(org.hibernate.jpa.HibernatePersistenceProvider.class); //If your using eclipse or change it to whatever you're using
         emf.setPackagesToScan("com.reflectme.server"); //The packages to search for Entities, line required to avoid looking into the persistence.xml
         emf.setPersistenceUnitName("PERSISTENCE");
         emf.setDataSource(dataSource);
-        emf.setLoadTimeWeaver(new ReflectiveLoadTimeWeaver()); //required unless you know what your doing
-        return (EntityManagerFactory)emf;
+        emf.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver()); //required unless you know what your doing
+        return emf;
     }
 
 }
