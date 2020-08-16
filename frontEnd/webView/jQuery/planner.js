9454f9ad506a3e7e59f7a5aad364d0145da81808
John Burns <johnb1003@gmail.com>
2020-08-16 15:19:28 -0400

    Planner Front end data retrieval

--- frontEnd/webView/jQuery/planner.js
+++ frontEnd/webView/jQuery/planner.js
@@ -657,6 +657,21 @@ function displayWeeks(weeks) {
         showWeeksHTML += '<p class="week-name">'+element.weekName+'</p> <div class="week-buttons">'; 
         showWeeksHTML += '<button class="edit-week-button" id="'+element.weekID+'">Ed.</button>';
         showWeeksHTML += '<button class="delete-week-button" id="'+element.weekID+'">Del</button> </div> </div>'
+        $('#'+element.weekID+' .week-checkbox').change( (e) => {
+            console.log("Here: "+$(e.target).attr('id'));
+            let currWeekID = $(e.target).attr('id');
+            let weeksArr = allMonthData.weeks;
+            weeksArr.forEach(element => {
+                if(element.weekID == parseInt(currWeekID)) {
+                    element.active = $('#'+currWeekID+' .week-checkbox').is(':checked');
+                    if(!updateWeekObject(element)) {
+                        // Error updating
+                        element.active = !$('#'+currWeekID+' .week-checkbox').is(':checked');
+                        $('#'+currWeekID+' .week-checkbox').prop( "checked", $('#'+currWeekID+' .week-checkbox').is(':checked'));
+                    }
+                }
+            });
+        });
     });
 
     $('.show-weeks-container').append(showWeeksHTML);
@@ -708,6 +723,7 @@ $(document).ready(function() {
     });
 
     // Side bar show specific week events (on calendar) buttons
+    /*
     $('.week-display-check input[type=checkbox]').change( (e) => {
         console.log("Here: "+$(e.target).attr('id'));
         let currWeekID = $(e.target).attr('id');
@@ -723,6 +739,7 @@ $(document).ready(function() {
             }
         });
     });
+    */
 
     $('#new-event-button').click( () => {
         $('.existing-events').css('display', 'none');
