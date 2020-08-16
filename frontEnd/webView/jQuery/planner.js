dbfd6f2db05817fb41f9e479e26eaf05b0a178cd
John Burns <johnb1003@gmail.com>
2020-08-16 15:41:44 -0400

    Planner Front end data retrieval

--- frontEnd/webView/jQuery/planner.js
+++ frontEnd/webView/jQuery/planner.js
@@ -653,7 +653,7 @@ function displayWeeks(weeks) {
             checked = 'checked';
         }
         showWeeksHTML += '<div class="week-row" id="week-'+element.weekID+'"> <div class="week-display-check"> '
-        showWeeksHTML += '<input type="checkbox" class="week-checkbox" id="week-check-'+element.weekID+'" '+checked+'></div>';
+        showWeeksHTML += '<input type="checkbox" class="week-checkbox" id="'+element.weekID+'" '+checked+'></div>';
         showWeeksHTML += '<p class="week-name">'+element.weekName+'</p> <div class="week-buttons">'; 
         showWeeksHTML += '<button class="edit-week-button" id="'+element.weekID+'">Ed.</button>';
         showWeeksHTML += '<button class="delete-week-button" id="'+element.weekID+'">Del</button> </div> </div>'
@@ -724,24 +724,6 @@ $(document).ready(function() {
         calendar.render();
     });
 
-    // Side bar show specific week events (on calendar) buttons
-    /*
-    $('.week-checkbox').change( (e) => {
-        console.log("Here: "+$(e.target).attr('id'));
-        let currWeekID = $(e.target).attr('id');
-        let weeksArr = allMonthData.weeks;
-        weeksArr.forEach(element => {
-            if(element.weekID == parseInt(currWeekID)) {
-                element.active = $('#'+currWeekID+' .week-checkbox').is(':checked');
-                if(!updateWeekObject(element)) {
-                    // Error updating
-                    element.active = !$('#'+currWeekID+' .week-checkbox').is(':checked');
-                    $('#'+currWeekID+' .week-checkbox').prop( "checked", $('#'+currWeekID+' .week-checkbox').is(':checked'));
-                }
-            }
-        });
-    });
-    */
 
     $('#new-event-button').click( () => {
         $('.existing-events').css('display', 'none');
