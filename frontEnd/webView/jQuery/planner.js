a1e301d122ff56db0f6255214e632a6dc4de8096
John Burns <johnb1003@gmail.com>
2020-08-20 10:11:16 -0400

    Scheduler Frontend design - clearFormData()

--- frontEnd/webView/jQuery/planner.js
+++ frontEnd/webView/jQuery/planner.js
@@ -644,7 +644,7 @@ function clearFormData() {
     let checkedLifts = $('.lift-type-pair input[type=checkbox]:checked');
     console.log(checkedLifts);
     for(let i=0; i<checkedLifts.length; i++) {
-        checkedLifts[i].prop('checked', false);
+        $('#'+checkedLifts[i].value.toLowerCase()).prop('checked', false);
     }
     $('#strength-other-name').val('Other');
 
