2d858d16ebfb7593dd8373c0f2f2549ff2d00f8e
John Burns <johnb1003@gmail.com>
2020-08-21 11:28:42 -0400

    Scheduler Frontend design - create events

--- frontEnd/webView/jQuery/planner.js
+++ frontEnd/webView/jQuery/planner.js
@@ -646,6 +646,12 @@ function processDayView() {
         }
         else if(id.includes('strength')) {
             idNum = id.substring(19);
+            console.log("Strengthid: "+idNum);
+            console.log(calendar.getFirstDayDateString());
+            console.log(calendar.selectedDate[2]);
+            console.log(allMonthData.months[calendar.getFirstDayDateString()]);
+            console.log(allMonthData.months[calendar.getFirstDayDateString()][''+calendar.selectedDate[2]]);
+            console.log(allMonthData.months[calendar.getFirstDayDateString()][''+calendar.selectedDate[2]]['strength']);
             eventArr = allMonthData.months[calendar.getFirstDayDateString()][''+calendar.selectedDate[2]]['strength'];
             eventArr.forEach( element => {
                 if(element.strengthid == idNum) {
